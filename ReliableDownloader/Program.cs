ee1cc3fd6c100282263726c05c7cea6d72b253af
hayden-accurx <93133777+hayden-accurx@users.noreply.github.com>
2022-05-26 10:14:49 +0100

    Update to latest installer

--- ReliableDownloader/Program.cs
+++ ReliableDownloader/Program.cs
@@ -9,7 +9,7 @@ namespace ReliableDownloader
         public static async Task Main(string[] args)
         {
             // If this url 404's, you can get a live one from https://installerstaging.accurx.com/chain/latest.json.
-            var exampleUrl = "https://installerstaging.accurx.com/chain/3.55.11050.0/accuRx.Installer.Local.msi";
+            var exampleUrl = "https://installerstaging.accurx.com/chain/3.182.57641.0/accuRx.Installer.Local.msi";
             var exampleFilePath = "C:/Users/[USER]/myfirstdownload.msi";
             var fileDownloader = new FileDownloader();
             await fileDownloader.DownloadFile(exampleUrl, exampleFilePath, progress => { Console.WriteLine($"Percent progress is {progress.ProgressPercent}"); });
